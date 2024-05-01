6c5fa9d67b2f352ff85b2c0c772f7ea580348747
Kakeru <stand.by.we@gmail.com>
2024-05-01 10:08:56 +0900

    gawa

---
+++ TravelBrochure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelBrochure
+//
+//  Created by Kakeru Fukuda on 2024/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelBrochure/TravelBrochureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBrochureApp.swift
+//  TravelBrochure
+//
+//  Created by Kakeru Fukuda on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct TravelBrochureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
